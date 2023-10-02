425845ca6d5c1c64f6e36968a20985e7a8659106
ApioMan3 <141684285+ApioMan3@users.noreply.github.com>
2023-10-02 19:50:27 -0300

    Conexion y ProductoData
    Método en ProductoData para agregar un producto

--- src/accesoADatos/Conexion.java
+++ src/accesoADatos/Conexion.java
@@ -1,14 +1,41 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package accesoADatos;
 
-/**
- *
- * @author ApioMan
- */
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import javax.swing.JOptionPane;
+
+
 public class Conexion {
-    
+
+    private static final String URL = "jdbc:mariadb://localhost/";
+    private static final String DB = "compras";
+    private static final String USUARIO = "root";
+    private static final String PASSWORD = "";
+    private static Connection connection;
+
+    public Conexion() {
+    }
+
+    public static Connection getConexion() {
+
+        if (connection == null) {
+
+            try {
+                Class.forName("org.mariadb.jdbc.Driver");
+                connection = DriverManager.getConnection(URL + DB, USUARIO, PASSWORD);
+            } catch (ClassNotFoundException ex) {
+                JOptionPane.showMessageDialog(null, "Error al cargar el driver.");
+            } catch (SQLException ex) {
+                JOptionPane.showMessageDialog(null, "Error en la conexión.");
+            }
+
+         
+        }
+
+        return connection;
+    }
+
 }
+
