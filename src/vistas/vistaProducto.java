5f6878273c37723c9c3ad1fbc2123dcf0b44953c
TashBrk <141684863+TashBrk@users.noreply.github.com>
2023-10-02 19:51:54 -0300

    vistas I
    vistas I

--- src/vistas/vistaProducto.java
+++ src/vistas/vistaProducto.java
@@ -183,7 +183,7 @@ public class vistaProducto extends javax.swing.JFrame {
     private void jTextField1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jTextField1ActionPerformed
         // TODO add your handling code here:
     }//GEN-LAST:event_jTextField1ActionPerformed
-
+//iiiii
     private void jButton3ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton3ActionPerformed
         // TODO add your handling code here:
     }//GEN-LAST:event_jButton3ActionPerformed
